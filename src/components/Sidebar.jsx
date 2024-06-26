ed1d7ab8f515cedf358efa10a21395dc6317caa7
busra <b.busraayilmaz@gmail.com>
2024-06-26 15:49:59 +0300

    feat: Add sign-in and sign-out functionality to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 import { FaXTwitter } from "react-icons/fa6";
 import { HiHome } from "react-icons/hi";
 function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="flex flex-col gap-4 p-3 ">
       <Link href="/">
@@ -16,9 +19,25 @@ function Sidebar() {
         <span className="font-bold hidden xl:inline">Home</span>
       </Link>
 
-      <button className="bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold">
-        Sign In
-      </button>
+      {session ? (
+        <button
+          className="bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold"
+          onClick={() => {
+            signOut();
+          }}
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          className="bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold"
+          onClick={() => {
+            signIn();
+          }}
+        >
+          Sign In
+        </button>
+      )}
     </div>
   );
 }
