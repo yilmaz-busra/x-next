a99aa6fcbf90facad5a268256c751f5d46400a21
busra <b.busraayilmaz@gmail.com>
2024-06-26 19:22:43 +0300

    chore: Update page layout to include sticky header and input component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import Input from "@/components/Input";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Page</h1>
+    <div className="max-w-xl min-h-screen mx-auto border-r border-l ">
+      <div className="py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+        <h2 className="text-lg sm:text-xl font-bold"> Home</h2>
+      </div>
+      <Input />
     </div>
   );
 }
