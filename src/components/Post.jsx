c4b929aa6c4921a0241da872abc8e98dfef2364f
busra <b.busraayilmaz@gmail.com>
2024-06-30 02:42:51 +0300

    Update font size

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,7 +13,7 @@ export default function Post({ post, id }) {
       <div className="flex-1">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-1 whitespace-nowrap">
-            <h4 className="font-bold text-sx truncate ">{post?.name}</h4>
+            <h4 className="font-bold text-sm truncate ">{post?.name}</h4>
             <span className="text-sx truncate">@{post?.username}</span>
           </div>
           <HiDotsHorizontal className="text-sm" />
