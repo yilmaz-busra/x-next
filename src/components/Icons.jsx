d7cf3d5dcd8f86686559661becd9714541ff0fd1
busra <b.busraayilmaz@gmail.com>
2024-06-30 03:19:26 +0300

    Added like functionality to Icons component

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,12 +1,75 @@
 "use client";
+import {
+  HiOutlineChat,
+  HiOutlineHeart,
+  HiOutlineTrash,
+  HiHeart,
+} from "react-icons/hi";
+import {
+  Timestamp,
+  collection,
+  deleteDoc,
+  doc,
+  getFirestore,
+  onSnapshot,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { signIn, useSession } from "next-auth/react";
+import { app } from "../firebase";
+import { useEffect, useState } from "react";
 
-import { HiOutlineChat, HiOutlineHeart, HiOutlineTrash } from "react-icons/hi";
+function Icons({ id }) {
+  const { data: session } = useSession();
+  const [isLiked, setIsLiked] = useState(false);
+  const [likes, setLikes] = useState([]);
+  const db = getFirestore(app);
+  const likePost = async () => {
+    if (session) {
+      if (isLiked) {
+        await deleteDoc(doc(db, "posts", id, "likes", session.user.uid));
+      } else {
+        await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+          username: session.user.username,
+          Timestamp: serverTimestamp(),
+        });
+      }
+    } else {
+      signIn();
+    }
+  };
 
-function Icons() {
+  useEffect(() => {
+    onSnapshot(collection(db, "posts", id, "likes"), (snapshot) => {
+      setLikes(snapshot.docs);
+    });
+  }, [db]);
+
+  useEffect(() => {
+    setIsLiked(likes.findIndex((like) => like.id === session?.user.uid) !== -1);
+  }, [likes]);
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
       <HiOutlineChat className="h-8 w-8 p-2 cursor-pointer rounded-full transition duration-500 ease-in-out hover:text-sky-500 hover:bg-sky-100" />
-      <HiOutlineHeart className="h-8 w-8 p-2 cursor-pointer rounded-full transition duration-500 ease-in-out hover:text-red-500 hover:bg-red-100" />
+      <div className="flex items-center">
+        {isLiked ? (
+          <HiHeart
+            onClick={likePost}
+            className="h-8 w-8 p-2 cursor-pointer rounded-full transition duration-500 ease-in-out hover:text-red-500 hover:bg-red-100"
+          />
+        ) : (
+          <HiOutlineHeart
+            onClick={likePost}
+            className="h-8 w-8 p-2 cursor-pointer rounded-full transition duration-500 ease-in-out  hover:text-red-500 hover:bg-red-100"
+          />
+        )}
+        {likes.length > 0 && (
+          <span className={`text-xs ${isLiked && "text-red-600"}`}>
+            {likes.length}
+          </span>
+        )}
+      </div>
+
       <HiOutlineTrash className="h-8 w-8 p-2 cursor-pointer rounded-full transition duration-500 ease-in-out hover:text-red-500 hover:bg-red-100" />
     </div>
   );
