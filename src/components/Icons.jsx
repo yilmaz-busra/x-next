d49658923b06a29ee30f0b8e54999b4355486daf
busra <b.busraayilmaz@gmail.com>
2024-07-01 23:17:39 +0300

    Added delete functionality to Icons component

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -19,7 +19,7 @@ import { signIn, useSession } from "next-auth/react";
 import { app } from "../firebase";
 import { useEffect, useState } from "react";
 
-function Icons({ id }) {
+function Icons({ id, uid }) {
   const { data: session } = useSession();
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState([]);
@@ -48,6 +48,23 @@ function Icons({ id }) {
   useEffect(() => {
     setIsLiked(likes.findIndex((like) => like.id === session?.user.uid) !== -1);
   }, [likes]);
+
+  const deletePost = async () => {
+    if (session?.user.uid === uid) {
+      if (window.confirm("Are you sure you want to delete this post?")) {
+        await deleteDoc(doc(db, "posts", id))
+          .then(() => {
+            console.log("Document successfully deleted!");
+            window.location.reload();
+          })
+          .catch((error) => {
+            console.error("Error removing document: ", error);
+          });
+      }
+    } else {
+      alert("You are not authorized to delete this post");
+    }
+  };
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
       <HiOutlineChat className="h-8 w-8 p-2 cursor-pointer rounded-full transition duration-500 ease-in-out hover:text-sky-500 hover:bg-sky-100" />
@@ -70,7 +87,12 @@ function Icons({ id }) {
         )}
       </div>
 
-      <HiOutlineTrash className="h-8 w-8 p-2 cursor-pointer rounded-full transition duration-500 ease-in-out hover:text-red-500 hover:bg-red-100" />
+      {session?.user.uid === uid && (
+        <HiOutlineTrash
+          onClick={deletePost}
+          className="h-8 w-8 p-2 cursor-pointer rounded-full transition duration-500 ease-in-out hover:text-red-500 hover:bg-red-100"
+        />
+      )}
     </div>
   );
 }
