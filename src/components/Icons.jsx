b63f2ea931b98032c4d9d6749425ce724496dba0
busra <b.busraayilmaz@gmail.com>
2024-07-02 23:19:42 +0300

    chore: Update Icons component to handle comment modal state and postId

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -19,13 +19,14 @@ import { signIn, useSession } from "next-auth/react";
 import { app } from "../firebase";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
-import { modalState } from "@/atom/modelAtom";
+import { modalState, postIdState } from "@/atom/modelAtom";
 
 function Icons({ id, uid }) {
   const { data: session } = useSession();
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState([]);
   const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
   const db = getFirestore(app);
   const likePost = async () => {
     if (session) {
@@ -71,7 +72,14 @@ function Icons({ id, uid }) {
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
       <HiOutlineChat
-        onClick={() => setOpen(!open)}
+        onClick={() => {
+          if (!session) {
+            signIn();
+          } else {
+            setOpen(!open);
+            setPostId(id);
+          }
+        }}
         className="h-8 w-8 p-2 cursor-pointer rounded-full transition duration-500 ease-in-out hover:text-sky-500 hover:bg-sky-100"
       />
       <div className="flex items-center">
