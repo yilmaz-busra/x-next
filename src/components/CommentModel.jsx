9c9264609586107136f5c714ec18cba1929674a0
busra <b.busraayilmaz@gmail.com>
2024-07-02 23:19:35 +0300

    feat: Update CommentModel to display postId when modal is open

--- src/components/CommentModel.jsx
+++ src/components/CommentModel.jsx
@@ -1,13 +1,14 @@
 "use client";
 import { useRecoilState } from "recoil";
-import { modalState } from "@/atom/modelAtom";
+import { modalState, postIdState } from "@/atom/modelAtom";
 
 export default function CommentModel() {
   const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
   return (
     <div>
       <h1>Comment Model</h1>
-      {open && <h1>The modal is open </h1>}
+      {open && <h1>{postId} </h1>}
     </div>
   );
 }
