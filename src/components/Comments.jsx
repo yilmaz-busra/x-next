8a7fed1e1cdcb9ee5f37138949e93dc91f6f0a62
busra <b.busraayilmaz@gmail.com>
2024-07-07 20:58:31 +0300

    Created Comments component to display comments on PostPage

---
+++ src/components/Comments.jsx
@@ -0,0 +1,34 @@
+"use client";
+import { app } from "../firebase";
+import {
+  collection,
+  getFirestore,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import Comment from "./Comment";
+
+export default function Comments({ id }) {
+  const db = getFirestore(app);
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db, id]);
+  return (
+    <div>
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment.data()} id={comment.id} />
+      ))}
+    </div>
+  );
+}
